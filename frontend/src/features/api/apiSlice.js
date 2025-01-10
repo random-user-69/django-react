83f3df07e519fb6310fa2e7c4730a1f33e1ce3e5
random-user-69 <a7925494@gmail.com>
2025-01-10 11:07:46 +0600

    Update apiSlice.js

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500/" }),
   endpoints: (builder) => ({}),
 });
